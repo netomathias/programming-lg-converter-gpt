a6c54ebbae4f6dd4a2bb5b412fd4a7c46bd6c8c1
ivonir_mathias_neto <ivonir.mathiasneto@gmail.com>
2023-04-30 08:05:38 -0300

    :wrench: chore: adjust in model e messages from gpt

--- gpt-function.mjs
+++ gpt-function.mjs
@@ -1,17 +1,22 @@
+import config from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
 
+config.config({
+  path: "./.env",
+});
+
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.API_TOKEN,
 });
 
 const openai = new OpenAIApi(configuration);
 
 export async function responseGpt(programmingLanguage, code) {
-  return await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `converta esse código de ${programmingLanguage} para java: ${code} devidamente`,
-    temperature: 0,
-    max_tokens: 150,
+  return await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: `converta esse código de ${programmingLanguage} para java: ${code} devidamente` }],
+    temperature: 0.5,
+    max_tokens: 200,
     top_p: 1.0,
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
